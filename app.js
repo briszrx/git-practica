ef77e5b4ec69e67ba68c54fa3d2f68da10647d30
briszrx <allison.avila@iteso.mx>
2026-09-04 19:17:47 -0600

    agregar func saludooo

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 console.log('Hola desde Git');
 console.log('Hola desde Git');
-console.log('Aprendiendo control de versiones');
+console.log('Aprendiendo control de versionesss');
+
+function saludar(nombre) {
+  return `Hola ${nombre}`;
+}
+
+console.log(saludar('Gitt'));
